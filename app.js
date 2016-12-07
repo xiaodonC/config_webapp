0bda411fa56239ddfa5eebbb1c05e0d3ae42afe8
chen-xd <chenxd@cn.ibm.com>
2016-12-07 16:05:15 +0800

    add query base page

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', login);
 app.use('/login', login);
-app.use('/users', users);
 app.use('/query', query);
+app.use('/index', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/login.js
+++ routes/login.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Config Cognitive Application' });
 });
 
+router.post('/', function(req, res, next) {
+  var user = {
+       username : 'admin',
+       password : 'admin123'
+  }
+  if(req.body.username===user.username && req.body.password===user.password){
+    res.send({
+        code : 0,
+        redirect : '/query'
+    });
+  }else{
+    res.send({
+        code : 1,
+        redirect : '/login'
+    });
+  }
+
+});
+
 module.exports = router;
