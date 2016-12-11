485b54ec4ea2a102b6aaa5350c1558105e33a7c5
ChenXiaoMian <236273522@qq.com>
2016-12-11 20:50:23 +0800

    增加内嵌页路由及页面

--- routes/query.js
+++ routes/query.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('query', { title: 'Config Cognitive Application' });
 });
 
+router.get('/mfc', function(req, res, next) {
+  res.render('query/mfc', { title: 'MFC Sourse Analyze' });
+});
+
+router.get('/sova', function(req, res, next) {
+  res.render('query/sova', { title: 'SOVA Sourse Analyze' });
+});
+
+router.get('/bi', function(req, res, next) {
+  res.render('query/bi', { title: 'BI Version Analyze' });
+});
+
+router.get('/watson', function(req, res, next) {
+  res.render('query/watson', { title: 'Watson Rebot' });
+});
+
 module.exports = router;
